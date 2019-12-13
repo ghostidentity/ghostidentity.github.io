a2ddaf74e66f78f3f9213413efe599d6a787b4ad
Mark Tagab <proxyshadow@gmail.com>
2019-12-13 20:58:26 +0800

    major update

--- assets/js/savetoFirebase.js
+++ assets/js/savetoFirebase.js
@@ -1,15 +1,22 @@
-const createMessage = function saveToFirebase(email, message) {
+const createMessage = async function saveToFirebase(name, email, subject, message) {
+
+    var result = false;
     var messageObject = {
+        name: name,
         email: email,
+        subject: subject,
         message: message
     }
 
-    firebase.database().ref('clientsMessage').push().set(messageObject)
-        .then(function(snapshot) {
-            console.log("Message sent to firebase db")
+    firebase.database().ref('message').push().set(messageObject)
+        .then(function() {
+             result = true;
+            console.log("Message sent to firebase db.");
         }, function(error) {
             console.log('error' + error);
-        });
+    });
+    
+   return await result
 
 }
 
